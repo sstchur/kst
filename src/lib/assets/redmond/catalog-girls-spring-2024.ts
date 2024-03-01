16e5ab8ac164c797926dd1874bbbd614d6346605
Stephen Stchur <sstchur@yahoo.com>
2024-03-01 15:37:51 -0800

    Redmond close date 3/1/24 at midnight

--- src/lib/assets/redmond/catalog-girls-spring-2024.ts
+++ src/lib/assets/redmond/catalog-girls-spring-2024.ts
@@ -107,4 +107,4 @@ export const logo = logo2023;
 export const taxRate = 0.101;
 export const markup = 3.00;
 export const payPalEnabled = true;
-export const closeDate = new Date(2024, 2, 0, 59, 59);
+export const closeDate = new Date(2024, 2, 1, 23, 59, 59);

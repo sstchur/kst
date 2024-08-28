52cd8d48cbdf095aa1c13c549bc3286fcb7b7e2a
Stephen Stchur <stchur@microsoft.com>
2024-08-27 19:26:42 -0700

    Update Juanita T to black

--- src/lib/assets/juanita/catalog-boys-fall-2024.ts
+++ src/lib/assets/juanita/catalog-boys-fall-2024.ts
@@ -1,16 +1,16 @@
 import type { Product } from "$lib/product";
 
-import pc380Front from '$lib/assets/juanita/img/boys/fall2024/pc380_deep-navy_front.png';
+import st350Front from '$lib/assets/juanita/img/boys/fall2024/st350_black_front.png';
 
 import pc78hFront from '$lib/assets/juanita/img/boys/fall2024/pc78h_dark-heather-grey_front.png';
 
 import st800Front from '$lib/assets/juanita/img/boys/fall2024/st800_true-navy-white_front.png';
 
-const pc380: Product = {
-    id: 'pc380-deep-navy',
-    title: 'PC380: Port & Company Performance Tee, Deep Navy',
-    description: '3.8-ounce, 100% polyester, UPF rating of 50, removable tag',
-    images: [ pc380Front ],
+const st350: Product = {
+    id: 'st350-black',
+    title: 'ST350: Sport Tek Performance Tee, Black',
+    description: '3.8-ounce, 100% polyester, removable tag',
+    images: [ st350Front ],
     price: 19.95,
     required: true,
 };
@@ -26,7 +26,7 @@ const pc78h: Product = {
 
 const st800: Product = {
     id: 'st800-navy-white',
-    title: 'J343: Sport-Tek Full-zip Jacket w/ Embroidered Logo',
+    title: 'ST800: Sport-Tek Full-zip Jacket w/ Embroidered Logo',
     description: 'True navy / white, 100% polyester double knit, side slash pockets, tag free label',
     images: [ st800Front ],
     price: 38.95
@@ -43,7 +43,7 @@ export const markup = 0;
 export const payPalEnabled = true;
 export const closeDate = new Date(2025, 10, 27, 59, 59);
 export const products: Product[] = [
-    pc380,
+    st350,
     pc78h,
     st800
 ];

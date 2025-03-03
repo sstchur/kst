ec4c852ef692e45d45d12fed11cbff7d065c01db
sstchur <sstchur@yahoo.com>
2025-03-03 15:23:46 -0800

    Update Redmond Catalog for 2025

--- src/lib/assets/redmond/catalog-girls-spring-2025.ts
+++ src/lib/assets/redmond/catalog-girls-spring-2025.ts
@@ -18,83 +18,83 @@ export const products: Product[] = [
     {
         id: 'holloway222777-green',
         model: '222777-Green',
-        title: 'Ladies Holloway Training Tank, Green',
+        title: '222777: Ladies Holloway Training Tank, Green',
         description: 'Moisture wicking, 88% poly/12% elastane Coolcore®, stretchy fit',
         images: [holloway222777Green],
         restrictedSizes: new Set(['3X-Large', '4X-Large']),
-        price: 26.95
+        price: 29.00
     },
     {
         id: 'holloway222777-black',
         model: '222777-Black',
-        title: 'Ladies Holloway Training Tank, Black',
+        title: '222777: Ladies Holloway Training Tank, Black',
         description: 'Moisture wicking, 88% poly/12% elastane Coolcore®, stretchy fit',
         images: [holloway222777Black],
         restrictedSizes: new Set(['3X-Large', '4X-Large']),        
-        price: 26.95
+        price: 29.00
     },
     {
         id: 'augusta1732-green',
         model: '1732-Green',
-        title: 'Ladies Augusta Step-Back Jersey, Green',
+        title: '1732: Ladies Augusta Step-Back Jersey, Green',
         description: 'Moisture wicking, 100% polyester, ladies fit',
         images: [augusta1732Green],
         restrictedSizes: new Set(['3X-Large', '4X-Large']),        
-        price: 31.95
+        price: 28.00
     },
     {
         id: 'holloway221335-black',
         model: '221335-Black',
-        title: 'Ladies Holloway PR Max Track Jersey, Black',
+        title: '221335: Ladies Holloway PR Max Track Jersey, Black',
         description: 'Moisture wicking, 100% polyester, ladies fit',
         images: [holloway221335Black],
         restrictedSizes: new Set(['3X-Large', '4X-Large']),        
-        price: 31.95
+        price: 33.00
     },
     {
         id: 'augusta1790-green',
         model: '1790-Green',
-        title: 'Augusta Ladies NextGen Tee, Green',
+        title: '1790: Augusta Ladies NextGen Tee, Green',
         description: '100% polyester wicking knit with color secure® technology',
         images: [augusta1790Green],
         restrictedSizes: new Set(['4X-Large']),
-        price: 16.95
+        price: 16.00
     },
     {
         id: 'augusta1790-black',
         model: '1790-Black',
-        title: 'Augusta Ladies NextGen Tee, Black',
+        title: '1790: Augusta Ladies NextGen Tee, Black',
         description: '100% polyester wicking knit with color secure® technology',
         images: [augusta1790Black],
         restrictedSizes: new Set(['4X-Large']),
-        price: 16.95
+        price: 16.00
     },
     {
         id: 'augusta2797-black',
         model: '2797-Black',
-        title: 'Augusta Ladies Long Sleeve Tee, Black',
+        title: '2797: Augusta Ladies Long Sleeve Tee, Black',
         description: '100% polyester wicking knit with color secure® technology',
         images: [augusta2797Black],
         restrictedSizes: new Set(['4X-Large']),
-        price: 21.95
+        price: 20.00
     },
     {
         id: 'dt6100-green',
         model: 'dt6100-Green',
-        title: 'District Cotton Hoodie, Green',
+        title: 'DT6100: District Cotton Hoodie, Green',
         description: '8.8oz, 100% ring spun cotton face',
         images: [dt6100Green],
         customizable: 9,
-        price: 29.95
+        price: 27.00
     },
     {
         id: 'dt6100-black',
-        model: 'dt6100-Black',
+        model: 'DT6100: dt6100-Black',
         title: 'District Cotton Hoodie, Black',
         description: '8.8oz, 100% ring spun cotton face',
         customizable: 9,
         images: [dt6100Black],
-        price: 29.95
+        price: 27.00
     }
 ];
 
@@ -105,6 +105,6 @@ export const collectionName = 'redmond-girlsspring2025';
 import logo2023 from '$lib/assets/redmond/img/logo2023.png';
 export const logo = logo2023;
 export const taxRate = 0.103;
-export const markup = 3.00;
+export const markup = 5.00;
 export const payPalEnabled = true;
-export const closeDate = new Date(2024, 2, 7, 23, 59, 59);
+export const closeDate = new Date(2025, 2, 5, 23, 59, 59);

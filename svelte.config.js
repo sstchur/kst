2a53153da65f4d7b930a640251521a1664ae2d80
sstchur <sstchur@yahoo.com>
2025-08-22 22:17:37 -0700

    sigh

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { sveltePreprocess } from 'svelte-preprocess';
 const config = {
   preprocess: sveltePreprocess(),
   kit: {
-    adapter: vercel({ runtime: 'nodejs18.x' })
+    adapter: vercel()
   }
 };
 

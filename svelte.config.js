321640fde86774451e947c2055b009e590535995
sstchur <sstchur@yahoo.com>
2025-08-22 22:15:08 -0700

    🤞

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-import adapter from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
 import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: sveltePreprocess(),
-
   kit: {
-    adapter: adapter()
+    adapter: vercel({ runtime: 'nodejs18.x' })
   }
 };
 
